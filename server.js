35778587896455bedb752d31e8c02b7c5811b242
kmanadkat <kmanadkat@gmail.com>
2022-10-15 22:36:19 +0530

    Passing Env Vars To Docker Image

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log('Containers rule!');
+    console.log(`Food Env Var Value: ${process.env.FAVORITE_FOOD}`)
     await sleep(5000);
   }
 }
